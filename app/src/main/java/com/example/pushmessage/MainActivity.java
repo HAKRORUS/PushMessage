04e9beb7379ee3296341f2c906fb1699de33dddf
HAKRORUS <qwaszxcvfdertgb@gmail.com>
2023-10-06 16:14:13 +0300

    done

--- app/src/main/java/com/example/pushmessage/MainActivity.java
+++ app/src/main/java/com/example/pushmessage/MainActivity.java
@@ -6,46 +6,49 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    // Идентификатор уведомления
-//    private static final int NOTIFY_ID = 101;
-//    InstanceID myID = InstanceID.getInstance(this);
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
 
-    // Идентификатор канала
-//    private static String CHANNEL_ID = "SHAILYSHAI channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NotificationChannel channel = new NotificationChannel("myCh", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myCh")
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setContentTitle("SHAILYSHAI")
+                .setContentText("You are Shailyshai");
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
     }
 
 
-//    public void Send(View view) {
-//        Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
-//        PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this,
-//                0, notificationIntent,
-//                PendingIntent.FLAG_CANCEL_CURRENT);
-//
-//
-//        NotificationCompat.Builder builder =
-//                new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-//                        .setContentTitle("Шайлушай")
-//                        .setSmallIcon(R.drawable.ic_launcher_foreground)
-//                        .setContentText("Вы стали ")
-//                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                        .setContentIntent(contentIntent);
-//
-//
-//        NotificationManagerCompat notificationManager =
-//                NotificationManagerCompat.from(MainActivity.this);
-//        notificationManager.notify(NOTIFY_ID, builder.build());
-//    }
+    public void Send(View view) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManagerCompat.notify(1, notification);
+    }
 }
